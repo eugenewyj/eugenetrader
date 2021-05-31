501f47fc720380822ccf9eee62619c1979bf48b7
eugene.wang <eugene.wagn@qunar.com>
2021-05-31 19:51:47 +0800

    remove vscode setting

--- quick_start_guide.py
+++ quick_start_guide.py
@@ -1,10 +1,39 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
+import datetime
+import os.path
+import sys
+
 import backtrader as bt
-from backtrader import cerebro
+
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function for this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+
 
 if __name__ == '__main__':
+    # Create a cerebro entity
     cerebro = bt.Cerebro()
+
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    print(modpath)
+    datapath = os.path.join(modpath, '../backtrader/datas/orcl-1995-2014.txt')
+    print(datapath)
+    # datapath = os.path.realpath(datapath)
+    # print(datapath)
+
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        fromdate=datetime.datetime(2000, 1, 1),
+        todate=datetime.datetime(2000, 12, 31),
+        reversed=False
+    )
+
+    cerebro.adddata(data)
+
+    cerebro._broker.set_cash(100000.00)
 
     print('Starting Protfolio Value: %.2f' % cerebro._broker.get_value())
 
