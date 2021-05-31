0d3e4e7d86ee1b5bb8a8e33f4eb5b6cb989f8e17
eugene <eugene.wang@qunar.com>
2021-05-31 22:39:40 +0800

    add quick start guid code

--- quick_start_guide.py
+++ quick_start_guide.py
@@ -5,6 +5,7 @@ import os.path
 import sys
 
 import backtrader as bt
+from backtrader.functions import If
 
 class TestStrategy(bt.Strategy):
 
@@ -15,9 +16,53 @@ class TestStrategy(bt.Strategy):
 
     def __init__(self) :
         self.dataclose = self.datas[0].close
+        self.dataopen = self.datas[0].open
+
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+    
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm: %.2f' % (order.executed.price, order.executed.value, order.executed.comm))
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            elif order.issell():
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm: %.2f' % (order.executed.price, order.executed.value, order.executed.comm))
+            
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+        
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' % (trade.pnl, trade.pnlcomm))
 
     def next(self):
-        self.log('Close, %.2f' % self.dataclose[0])
+        self.log('Open, %.2f, Close, %.2f' % (self.dataopen[0], self.dataclose[0]))
+
+        if self.order:
+            return
+
+        if not self.position:
+            if self.dataclose[0] < self.dataclose[-1]:
+                if self.dataclose[-1] < self.dataclose[-2]:
+                    self.log('BUY CREATE %.2f' % self.dataclose[0])
+                    self.order = self.buy()
+        else:
+            if len(self) >= (self.bar_executed + 5):
+                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                 self.order = self.sell()
 
 
 
@@ -44,6 +89,8 @@ if __name__ == '__main__':
     cerebro.adddata(data)
 
     cerebro._broker.set_cash(100000.00)
+
+    cerebro._broker.setcommission(0.001)
 
     print('Starting Protfolio Value: %.2f' % cerebro._broker.get_value())
 
