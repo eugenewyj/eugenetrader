e2f5f68ca5765da74e4ecb173e46593502fb680f
Eugene Wang <eugenewyj79@gmail.com>
2021-09-02 19:36:37 +0800

    添加股票DB模块

--- stock_db/stock_db.py
+++ stock_db/stock_db.py
@@ -6,8 +6,15 @@
 #
 ###############################################################################
 
+import sqlite3 as sqlite
+
 def init_ticker_list():
-    print("test")
+    con = sqlite.connect('/mnt/d/02Eugene/01Projects/trader_eugene/stock_db/stock_us.db')
+    cur = con.cursor()
+    # Create table
+    cur.execute('''CREATE TABLE stocks(symbol text, name text, lastsale real, netchange real, pctchange real, marketCap real, country text, ipoyear real, volume real, sector text, industry text, url text)''')
+    cur.commit()
+
 
 
 if __name__ == '__main__':
